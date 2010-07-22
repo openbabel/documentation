37a90fb46a577deed9c6dd1ff2a48e39a84de98e
Noel O'Boyle <baoilleach@gmail.com>
2010-07-22 22:20:26 +0100

    Correct LaTeX

--- FixFileFormatHTML.py
+++ FixFileFormatHTML.py
@@ -11,4 +11,14 @@ for filename in glob.glob(os.path.join("_build", "html", "FileFormats", "*.html"
                 output.write("<kbd><span class=\"option\">" + line[27:])
             else:
                 output.write(line)
-    
+
+# Fix LaTeX
+filename = os.path.join("_build", "latex", "OpenBabel.tex")
+with open(filename, "r") as input:
+    text = input.readlines()
+with open(filename, "w") as output:
+    for line in text:
+        if line.startswith("\item [-"):
+            output.write("\item [" + line[8:])
+        else:
+            output.write(line)
