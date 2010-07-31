46e00ba4c07e8b8d34507cf83f2fdeca29b162a8
Noel O'Boyle <baoilleach@gmail.com>
2010-07-31 13:14:33 +0100

    Comment out the requirement for doxylink

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.doxylink']
+# extensions = ['sphinxcontrib.doxylink']
 
 doxylink = {'obapi' : ("tags", "_static\\API\\")}
 
