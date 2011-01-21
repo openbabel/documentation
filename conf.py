37df79b6d391f596dbb929854f9f9fa40f1591e4
Noel O'Boyle <baoilleach@gmail.com>
2011-01-21 11:14:25 +0000

    If doxylink extension is not available, just ignore silently.

--- conf.py
+++ conf.py
@@ -23,11 +23,17 @@ sys.path = ["."] + sys.path
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinxcontrib.doxylink'
     ]
+try:
+    import sphinxcontrib.doxylink
+except ImportError:
+    pass
+else:
+    extensions.append("sphinxcontrib.doxylink")
 
 doxylink = {'obapi' : ("tags", "http://openbabel.org/dev-api/")}
 
