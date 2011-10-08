90cf705247dff0da945daf0117d3b48f68e2485e
Noel O'Boyle <baoilleach@gmail.com>
2011-10-08 16:59:11 +0100

    Update copyright

--- conf.py
+++ conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open Babel'
-copyright = u'2010, Open Babel community'
+copyright = u'2011, Open Babel community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
